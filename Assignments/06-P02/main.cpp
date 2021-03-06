3aad33f90fc694a0d6281cc31ac529e126ae5b65
jonhogan <jon.hogan83@gmail.com>
2021-03-06 10:29:20 -0600

    Pushed from Tenjin

--- Assignments/06-P02/main.cpp
+++ Assignments/06-P02/main.cpp
@@ -15,11 +15,13 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <string>
 
 typedef std::vector<std::string> vs;
 
 
-int main() {
+int main()
+ {
     char k;                // holder for character being typed
     string word = "";      // var to concatenate letters to
     vs wordList;           // Location to read words into 
@@ -48,9 +50,9 @@ int main() {
 
     // print out how long it took to load the dictionary file
     std::cout << "It took " << timer.Seconds() << " seconds to read in the data" << std::endl;
-    cout << "It took " << timer.MilliSeconds() << " milliseconds to read in the data" << std::endl;
+    std::cout << "It took " << timer.MilliSeconds() << " milliseconds to read in the data" << std::endl;
 
-    cout << "Type keys and watch what happens. Type capital Z to quit." << endl;
+    std::cout << "Type keys and watch what happens. Type capital Z to quit." << std::endl;
 
     timer.Start();      // start it
 
@@ -66,28 +68,34 @@ int main() {
 
     // print out how long it took to load the linked list dictionary
     std::cout << "It took " << timer.Seconds() << " seconds to read in the data" << std::endl;
-    cout << "It took " << timer.MilliSeconds() << " milliseconds to read in the data" << std::endl;
+    std::cout << "It took " << timer.MilliSeconds() << " milliseconds to read in the data" << std::endl;
 
     // While capital Z is not typed keep looping
-    while ((k = getch()) != 'Z') {
+    while ((k = getch()) != 'Z')
+    {
         
         // Tests for a backspace and if pressed deletes
         // last letter from "word".
-        if ((int)k == 127 || k == 8) {
-            if (word.size() > 0) {
+        if ((int)k == 127 || k == 8)
+        {
+            if (word.size() > 0)
+            {
                 word = word.substr(0, word.size() - 1);
             }
-        } else {
+        }else
+        {
             // Make sure a letter was pressed and only letter
-            if (!isalpha(k)) {
-                cout << "Letters only!" << endl;
+            if (!isalpha(k))
+            {
+                std::cout << "Letters only!" << std::endl;
                 continue;
             }
 
             // We know its a letter, lets make sure its lowercase.
             // Any letter with ascii value < 97 is capital so we
             // lower it.
-            if ((int)k < 97) {
+            if ((int)k < 97)
+            {
                 k += 32;
             }
             word += k; // append char to word
@@ -98,8 +106,8 @@ int main() {
         timer.Start(); // start it
         matches = dictionary.search(word);
         timer.End(); // end the current timer
-        cout << timer.Seconds() << " seconds to read in and print" << endl;
-        cout << timer.MilliSeconds() << " milli to read in and print" << endl;
+        std::cout << timer.Seconds() << " seconds to read in and print" << std::endl;
+        std::cout << timer.MilliSeconds() << " milli to read in and print" << std::endl;
 
         if (word == "")
         {
@@ -107,16 +115,16 @@ int main() {
         }else if ((int)k != 32) 
         { 
             // if k is not a space print it
-            cout << "Keypressed: " << termcolor::on_yellow << termcolor::blue << k << " = " << (int)k << termcolor::reset << endl;
-            cout << "Current Substr: " << termcolor::red << word << termcolor::reset << endl;
-            cout << "Word found: ";
-            cout << termcolor::green;
+            std::cout << "Keypressed: " << termcolor::on_yellow << termcolor::blue << k << " = " << (int)k << termcolor::reset << std::endl;
+            std::cout << "Current Substr: " << termcolor::red << word << termcolor::reset << std::endl;
+            std::cout << "Word found: ";
+            std::cout << termcolor::green;
             // This prints out all found matches
             for (int i = 0; i < matches.size(); i++) {
-                cout << matches[i] << " ";
+                std::cout << matches[i] << " ";
             }
-            cout << termcolor::reset << endl
-                 << endl;
+            std::cout << termcolor::reset << std::endl
+                 << std::endl;
         }
 
     }
