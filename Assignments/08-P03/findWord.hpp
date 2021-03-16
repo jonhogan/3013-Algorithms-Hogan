8e1f533dfef749027711594139b7a7c0d45f0d87
jonhogan <jon.hogan83@gmail.com>
2021-03-15 21:43:18 -0500

    Pushed from Tenjin WIP

--- Assignments/08-P03/findWord.hpp
+++ Assignments/08-P03/findWord.hpp
@@ -1,11 +1,7 @@
 #pragma once
 #include <fstream>
 #include <iostream>
-#include <vector>
 #include <string>
-
-
-typedef std::vector<std::string> vs;
 
 
 /****************************************************************************
@@ -29,7 +25,7 @@ typedef std::vector<std::string> vs;
 * wordNode                                                            *
 *                                                                     *
 * Description:                                                        *
-*        Node structure for a linked list                             *
+*        wordNode structure to hold a word, and definition            *
 *                                                                     *
 * Private Methods:                                                    *
 *        None                                                         *
@@ -43,12 +39,22 @@ typedef std::vector<std::string> vs;
 struct wordNode
 {
     std::string word;
-    std::string def;
+    std::string definition;
+    wordNode* left;
+    wordNode* right;
 
     wordNode()
     {
         word = "";
-        def = "";
+        definition = "";
+        left = right = NULL;
+    }
+
+    wordNode(std::string w, std::string d)
+    {
+        word = w;
+        definition = d;
+        left = right = NULL;
     }
 
 };
@@ -60,7 +66,7 @@ struct wordNode
 * WordTree                                                            *
 *                                                                     *
 * Description:                                                        *
-*        Node structure for a linked list                             *
+*        Binary tree using a linked list                              *
 *                                                                     *
 * Private Methods:                                                    *
 *        None                                                         *
@@ -77,30 +83,98 @@ struct wordNode
 
 class WordTree
 {
-    protected:
-        wordNode* m_dictionary;
-        int m_currentSize;
-        float m_ratio;
+    private:
+        wordNode* m_root;
+        int m_size;
+        void Print(wordNode*);
 
+        void Insert(wordNode* &m_root, std::string w, std::string d)
+        {
+            bool didInsert = false;
+            
+            if(!m_root)
+            {
+                m_root = new wordNode(w, d);
+            }else
+            {
+                if (m_root -> word[0] == w[0])
+                {
+                    for (int i = 0; i < m_root -> word.size();i++)
+                    {
+                        if (m_root -> word[i] > w[i])
+                        {
+                            Insert(m_root -> left, w, d);
+                            didInsert = true;
+
+                        }else if (m_root -> word[i] < w[i])
+                        {
+                            Insert(m_root -> right, w, d);
+                            didInsert = true;
+                        }
+                    }
+                    if (!didInsert)
+                    {
+                        Insert(m_root -> right, w, d);
+                    }
+                    
+                }else if (m_root -> word[0] > w[0])
+                {
+                    Insert(m_root -> left, w, d);
+                }else
+                {
+                    Insert(m_root -> right, w, d);
+                }
+            }
+        }
+        bool Find(wordNode*,int);
+        int Height(wordNode*);
+        int NumChildren(wordNode*);
+        
     public:
+        WordTree()
+        {
+            m_size = 0;
+            m_root = NULL;
+        }
 
-        /**********************************************************************
-        *                                                                     *
-        * WordTree                                                            *
-        *                                                                     *
-        * Description:                                                        *
-        *        Default Constructor                                          *
-        *        Array based binary tree                                      *
-        *                                                                     *
-        * Method Variables:                                                   *
-        *        m_currentSize                                                *
-        *        m_dictionary                                                 *
-        *        m_ratio                                                      *
-        *                                                                     *
-        * Use:                                                                *
-        *   Create an array based binary tree                                 *
-        *                                                                     *
-        **********************************************************************/
+        /*WordTree(std::string *w, std::string *d)
+        {
+
+        }*/
+
+        void Insert(std::string w, std::string d)
+        {
+            if(!m_root)
+            {
+                wordNode* temp = new wordNode(w, d);
+                m_root = temp;
+            }else
+            {
+                Insert(m_root, w, d);
+            }
+        }
+        void Print();
+        bool Find(int);
+        void Delete();
+        int Height();
+        
+        /**************************************************************
+        *                                                             *
+        * WordTree                                                    *
+        *                                                             *
+        * Description:                                                *
+        *        Default Constructor                                  *
+        *        Array based binary tree                              *
+        *                                                             *
+        * Method Variables:                                           *
+        *        m_currentSize                                        *
+        *        m_dictionary                                         *
+        *        m_ratio                                              *
+        *                                                             *
+        * Use:                                                        *
+        *   Create an array based binary tree                         *
+        *                                                             *
+        **************************************************************/
 
         WordTree()
         {
@@ -110,18 +184,18 @@ class WordTree
         }
 
 
-        /**********************************************************************
-        * GrowTree                                                            *
-        *                                                                     *
-        * Description:                                                        *
-        *         If the binary tree is full, grow it                         *
-        *                                                                     *
-        * Params:                                                             *
-        *         NULL                                                        *
-        *                                                                     *
-        * Returns:                                                            *
-        *         NULL                                                        *
-        **********************************************************************/
+        /**************************************************************
+        * GrowTree                                                    *
+        *                                                             *
+        * Description:                                                *
+        *         If the binary tree is full, grow it                 *
+        *                                                             *
+        * Params:                                                     *
+        *         NULL                                                *
+        *                                                             *
+        * Returns:                                                    *
+        *         NULL                                                *
+        **************************************************************/
 
         void GrowTree()
         {
@@ -134,24 +208,25 @@ class WordTree
             }
 
             m_currentSize = growSize;          // Set current size to the new size
-            delete[] m_dictionary;                      // Delete old array
+            delete[] m_dictionary;             // Delete old array
             m_dictionary = B;                           // Reset the pointer
+            delete[] B;
         }
 
-        /*******************************************************************
-        *                                                                  *
-        * print                                                            *
-        *                                                                  *
-        * Description:                                                     *
-        *        Prints the BST                                            *
-        *                                                                  *
-        * Method Variables:                                                *
-        *        None                                                      *
-        *                                                                  *
-        * Return:                                                          *
-        *   NULL                                                           *
-        *                                                                  *
-        *******************************************************************/
+        /**************************************************************
+        *                                                             *
+        * print                                                       *
+        *                                                             *
+        * Description:                                                *
+        *        Prints the WordTree                                       *
+        *                                                             *
+        * Method Variables:                                           *
+        *        None                                                 *
+        *                                                             *
+        * Return:                                                     *
+        *   NULL                                                      *
+        *                                                             *
+        **************************************************************/
         /*
         void print()
         {
@@ -167,40 +242,40 @@ class WordTree
         
         }
         */
-        /*******************************************************************
-        *                                                                  *
-        * getSize                                                          *
-        *                                                                  *
-        * Description:                                                     *
-        *        Gets the size of the linked list                          *
-        *                                                                  *
-        * Method Variables:                                                *
-        *        None                                                      *
-        *                                                                  *
-        * Return:                                                          *
-        *   size                                                           *
-        *                                                                  *
-        *******************************************************************/
+        /**************************************************************
+        *                                                             *
+        * getSize                                                     *
+        *                                                             *
+        * Description:                                                *
+        *        Gets the size of the linked list                     *
+        *                                                             *
+        * Method Variables:                                           *
+        *        None                                                 *
+        *                                                             *
+        * Return:                                                     *
+        *   size                                                      *
+        *                                                             *
+        **************************************************************/
 /*
         int getSize()
         {
             return size;
         }
 */
-        /*******************************************************************
-        *                                                                  *
-        * insert                                                           *
-        *                                                                  *
-        * Description:                                                     *
-        *        Adds a word to the linked list                            *
-        *                                                                  *
-        * Method Variables:                                                *
-        *        None                                                      *
-        *                                                                  *
-        * Return:                                                          *
-        *   NULL                                                           *
-        *                                                                  *
-        *******************************************************************/
+        /**************************************************************
+        *                                                             *
+        * insert                                                      *
+        *                                                             *
+        * Description:                                                *
+        *        Adds a word to the linked list                       *
+        *                                                             *
+        * Method Variables:                                           *
+        *        None                                                 *
+        *                                                             *
+        * Return:                                                     *
+        *   NULL                                                      *
+        *                                                             *
+        **************************************************************/
 /*
         void insert(wordNode* word)
         {
@@ -216,20 +291,21 @@ class WordTree
             size++;
         }
 */
-        /*******************************************************************
-        *                                                                  *
-        * search                                                           *
-        *                                                                  *
-        * Description:                                                     *
-        *        searches through the liked list to find a matching word   *
-        *                                                                  *
-        * Method Variables:                                                *
-        *        vector <string> (vs) results                              *
-        *                                                                  *
-        * Return:                                                          *
-        *   vector <string> (vs) results                                   *
-        *                                                                  *
-        *******************************************************************/
+        /**************************************************************
+        *                                                             *
+        * search                                                      *
+        *                                                             *
+        * Description:                                                *
+        *        searches through the liked list to find a matching   * 
+        *        word                                                 *
+        *                                                             *
+        * Method Variables:                                           *
+        *        vector <string> (vs) results                         *
+        *                                                             *
+        * Return:                                                     *
+        *   vector <string> (vs) results                              *
+        *                                                             *
+        **************************************************************/
     /*
         vs search(std::string userInput)
         {
